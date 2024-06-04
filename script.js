ac5908b24816a7b6d853a833602f2b4c6f308639
Isabela Melleis <isabela.zmelleis@gmail.com>
2024-06-03 21:57:38 -0300

    finalizando a compra dos cursores e adicionando o badzin
    Co-Authored-By: Lorenzo Broch <160625132+LorenzoBroch@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//quantidade absoluta de naldocoins na gameplay do usuário
 let NALDOCOIN = 0;
 
 
@@ -6,7 +7,7 @@ const botaoKamkeClicker = document.getElementById('kamke-clicker');
 
 
 // "p" do html onde está a contagem dos naldocoins
-let naldoCoins = document.getElementById('naldo-coins');
+let naldoCoins = document.getElementById('NALDOCOINS');
 
 
 // contagem dos clicks para contabilizar mais naldocoins
@@ -14,28 +15,78 @@ botaoKamkeClicker.addEventListener('click', contarClicks);
 function contarClicks() {
     NALDOCOIN++;
     naldoCoins.innerText = NALDOCOIN;
+    if (NALDOCOIN >= 100) {
+        desbloquearGustavo();
+    }
 }
 
-// ---------------------------------------------------------------------------
-// cursor - primeira compra para CURSOR
-let precoCursor = document.getElementById('precoCursor');
+// ------------------------------ CURSOR -------------------------------------
+// primeira compra para CURSOR
+let precoCursor = document.getElementById('preco-cursor');
 let precoCursorValor = 10;
 precoCursor.innerText = precoCursorValor;
-// ---------------------------------------------------------------------------
 
-const botaoComprarCursor = document.getElementById('compraCursor');
+const botaoComprarCursor = document.getElementById('compra-cursor');
+let quantCursor = 0;
 botaoComprarCursor.addEventListener('click', function() {
     if (NALDOCOIN < precoCursorValor) {
         window.alert("TOMA NO TEU CU");
-    } else { // Mudar Preço
+    } else {
+
+        // altera a contagem dos NALDOCOINS depois da compra
         NALDOCOIN = NALDOCOIN - precoCursorValor;
         naldoCoins.innerText = NALDOCOIN;
-        precoCursorValor =(precoCursorValor*2);
+
+        // aumenta o valor do cursor para uma próxima compra
+        precoCursorValor = Math.trunc(precoCursorValor + (precoCursorValor * 0.6)); // Math.trunc tira todas as casas depois da vírgula
         precoCursor.innerText = precoCursorValor;
-        let divCursorComprado = document.getElementById('displayNone');
+
+        // faz aparecer a div com a imagem do cursor
+        let divCursorComprado = document.getElementById('div-cursor-comprado');
         divCursorComprado.style.display = 'flex';
-        divCursorComprado.classList.add('divCursorComprado');
+        divCursorComprado.classList.add('div-cursor-comprado');
+
+        // faz o cursor (que acabou de ser comprado) começar a fazer os autoclicks
+        function autoClick() {
+            botaoKamkeClicker.click();
+        }
+        setInterval(autoClick, 1500);
+        
+        //aumenta a quantidade de cursores possuídos a cada compra
+        quantCursor++;
+
+        //aumenta e mostra o numero de cursores comprados
+        const stacksDeCursors = document.getElementById('stacks-cursors');
+        stacksDeCursors.innerText = `${quantCursor}x`;
+}});
+
+// ------------------------------ GUSTAVO VENTURI ----------------------------------
+// primeira compra para BADZIN
+let precoGustavo = document.getElementById('preco-gustavo');
+let precoGustavoValor = 100;
+precoGustavo.innerHTML = precoGustavoValor;
+
+function desbloquearGustavo() {
+    let badzinNaoDesbloqueado = document.getElementById('badzin-nao-desbloqueado');
+    badzinNaoDesbloqueado.style.display = 'none';
+
+    let gustavoDesbloqueado = document.getElementById('badzin-desbloqueado');
+    gustavoDesbloqueado.style.display = 'flex';
+}
+
+const botaoBadzin = document.getElementById('compra-gustavo');
+botaoBadzin.addEventListener('click', function() {
+    if (NALDOCOIN < precoGustavoValor) {
+        window.alert("TOMA NO TEU CU");
+    } else {
+
+        // altera a contagem dos NALDOCOINS depois da compra
+        NALDOCOIN = NALDOCOIN - precoGustavoValor;
+        naldoCoins.innerText = NALDOCOIN;
+
+        // aumenta o valor do badzin para uma próxima compra
+        precoGustavoValor = precoGustavoValor + (precoGustavoValor * 3);
+        precoGustavo.innerText = precoGustavoValor;
+        
     }
-});
-
-
+})
